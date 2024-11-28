54cbf4aabe5b2d01a6b772244ad052b9b8b8de52
Varren999 <varren999@gmail.com>
2024-11-28 20:11:23 +0200

    Work version

---
+++ Buckix_DZ2_String12/Program.cs
@@ -0,0 +1,53 @@
+﻿//=================================================================================================
+//  Дана непустая строка S и целое число N (>0). Вывести строку, содержащую символы строки S,
+//  между которыми вставлено по N символов «*» (звездочка).
+//=================================================================================================
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buckix_DZ2_String12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 0;
+            string text = "";
+            bool cycle = true;
+            Console.WriteLine("Вывести строку, содержащую символы строки S, между которыми вставлено по N символов «*» (звездочка).");
+            while (cycle)
+            {
+                cycle = false;
+                try
+                {
+                    Console.Write("Введите строку:");
+                    text = Console.ReadLine();
+                    Console.Write("Введите число N:");
+                    N = Convert.ToInt32(Console.ReadLine());
+                }
+                catch(Exception ex) 
+                {
+                    cycle = true;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message);
+                    Console.ResetColor();
+                }
+            }
+            Console.WriteLine(InsertStars(text, N));
+            Console.ReadKey();
+        }
+
+        static string InsertStars(string text, int num)
+        {
+            string stars = "";
+            if (num < 1)
+                return text;
+            for(int i = 0; i < num; i++)
+                stars += "*";
+            return string.Join(stars, text.ToCharArray());
+        }
+    }
+}
